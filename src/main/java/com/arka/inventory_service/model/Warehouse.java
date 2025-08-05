0ed5ea7017abf59e8499638fb8c16bdbf88c7c7d
Jose Navarro <jfnr398@gmail.com>
2025-08-05 17:39:36 -0500

    feat(model): create Warehouse entity class

---
+++ src/main/java/com/arka/inventory_service/model/Warehouse.java
@@ -0,0 +1,35 @@
+package com.arka.inventory_service.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "warehouse")
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 200)
+    private String location;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
+
