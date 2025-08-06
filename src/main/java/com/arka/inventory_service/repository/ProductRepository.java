77dfe2b616d3da8afabf85e175666160bd1810ec
Jose Navarro <jfnr398@gmail.com>
2025-08-06 16:32:46 -0500

    feat(repository): add methods to Product repository interface

--- src/main/java/com/arka/inventory_service/repository/ProductRepository.java
+++ src/main/java/com/arka/inventory_service/repository/ProductRepository.java
@@ -5,14 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    boolean existsByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCaseAndBrandId(String name, UUID brandId);
+    List<Product> findAllByNameIgnoreCase(String name);
+    List<Product> findAllByDescriptionIgnoreCase(String description);
     List<Product> findAllByBrandId(UUID brandId);
     List<Product> findAllByCategoryId(UUID categoryId);
-    List<Product> findAllByNameIgnoreCase(String name);
-    boolean existsByNameIgnoreCase(String name);
 }
 
