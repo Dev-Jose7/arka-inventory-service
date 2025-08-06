09d0b038df631d1c4ffd4655f9dcc2093d1283f1
Jose Navarro <jfnr398@gmail.com>
2025-08-06 16:31:47 -0500

    feat(repository): add methods to Country repository interface

--- src/main/java/com/arka/inventory_service/repository/CountryRepository.java
+++ src/main/java/com/arka/inventory_service/repository/CountryRepository.java
@@ -2,11 +2,13 @@ package com.arka.inventory_service.repository;
 
 import com.arka.inventory_service.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CountryRepository extends JpaRepository<Country, UUID> {
-    Optional<Country> findByName(String name);
     boolean existsByName(String name);
+    Optional<Country> findByName(String name);
 }
