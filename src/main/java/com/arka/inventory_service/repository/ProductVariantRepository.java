63a55e85d7cd1d46c09ea706e510d8d57cd4bc68
Jose Navarro <jfnr398@gmail.com>
2025-08-06 16:33:38 -0500

    feat(repository): add methods to ProductVariant repository interface

--- src/main/java/com/arka/inventory_service/repository/ProductVariantRepository.java
+++ src/main/java/com/arka/inventory_service/repository/ProductVariantRepository.java
@@ -1,18 +1,22 @@
 package com.arka.inventory_service.repository;
 
-import com.arka.inventory_service.model.Product;
 import com.arka.inventory_service.model.ProductVariant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductVariantRepository extends JpaRepository<ProductVariant, UUID> {
     boolean existsBySku(String sku);
-    long countByProduct(Product product);
-    List<ProductVariant> findByProductId(UUID productId);
-    List<ProductVariant> findByWarehouseId(UUID warehouseId);
+    boolean existsByNameIgnoreCaseAndProductId(String name, UUID productId);
+    long countByProductId(UUID productId);
+    Optional<ProductVariant> findBySku(String sku);
+    List<ProductVariant> findAllByNameIgnoreCase(String name);
+    List<ProductVariant> findAllByProductId(UUID productId);
+    List<ProductVariant> findAllByCurrencyId(UUID currencyId);
+    List<ProductVariant> findAllByPrice(BigDecimal price);
 }
-
