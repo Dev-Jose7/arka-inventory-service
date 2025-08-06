6240165251e51eb84dd1f3514e332da6725f901c
Jose Navarro <jfnr398@gmail.com>
2025-08-06 16:33:09 -0500

    feat(repository): add methods to Currency repository interface

--- src/main/java/com/arka/inventory_service/repository/CurrencyRepository.java
+++ src/main/java/com/arka/inventory_service/repository/CurrencyRepository.java
@@ -2,12 +2,15 @@ package com.arka.inventory_service.repository;
 
 import com.arka.inventory_service.model.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CurrencyRepository extends JpaRepository<Currency, UUID> {
+    boolean existsByNameIgnoreCase(String name);
     Optional<Currency> findByName(String name);
     Optional<Currency> findByCode(String name);
     List<Currency> findAllBySymbol(String symbol);
