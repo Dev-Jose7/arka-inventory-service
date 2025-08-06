e7ca353720d9a3b211d431d51fc83ca010b3552b
Jose Navarro <jfnr398@gmail.com>
2025-08-06 16:31:27 -0500

    feat(repository): add methods to Category repository interface

--- src/main/java/com/arka/inventory_service/repository/CategoryRepository.java
+++ src/main/java/com/arka/inventory_service/repository/CategoryRepository.java
@@ -1,16 +1,17 @@
 package com.arka.inventory_service.repository;
 
-import com.arka.inventory_service.model.Brand;
 import com.arka.inventory_service.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
-    Optional<Brand> findByNameIgnoreCase(String name);
     boolean existsByNameIgnoreCase(String name);
+    Optional<Category> findByNameIgnoreCase(String name);
+    List<Category> findAllByDescription(String description);
 }
 
