1a33fab0a714d0c99a2b0dcd6a2115389ca2a312
Jose Navarro <jfnr398@gmail.com>
2025-08-06 15:34:28 -0500

    feat(repository): add interface for Stock repository

---
+++ src/main/java/com/arka/inventory_service/repository/StockRepository.java
@@ -0,0 +1,15 @@
+package com.arka.inventory_service.repository;
+
+import com.arka.inventory_service.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, UUID> {
+    List<Stock> findAllByProductId(UUID productId);
+    List<Stock> findAllByWarehouse(UUID warehouseId);
+    List<Stock> findAllByStock(Integer stock);
+}
