9ba4194c0fc28ec285fb84cff6056342d85d1cb0
Jose Navarro <jfnr398@gmail.com>
2025-08-07 03:22:26 -0500

    feat(controller): add ProductController

---
+++ src/main/java/com/arka/inventory_service/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.arka.inventory_service.controller;
+
+import com.arka.inventory_service.dto.request.ProductRequestDTO;
+import com.arka.inventory_service.dto.response.ProductResponseDTO;
+import com.arka.inventory_service.service.IProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final IProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> createProduct(@Valid @RequestBody ProductRequestDTO request) {
+        ProductResponseDTO response = productService.createProduct(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getAllProducts() {
+        List<ProductResponseDTO> response = productService.getAllProducts();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<List<ProductResponseDTO>> getProductsByName(@PathVariable String name) {
+        List<ProductResponseDTO> response = productService.getProductsByName(name);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/brand/{brandId}")
+    public ResponseEntity<List<ProductResponseDTO>> getProductsByBrand(@PathVariable UUID brandId) {
+        List<ProductResponseDTO> response = productService.getProductByBrandId(brandId);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public ResponseEntity<List<ProductResponseDTO>> getProductsByCategory(@PathVariable UUID categoryId) {
+        List<ProductResponseDTO> response = productService.getProductByCategoryId(categoryId);
+        return ResponseEntity.ok(response);
+    }
+}
