fdeade715dc52157414f5ed81c44a004af92d85d
Jose Navarro <jfnr398@gmail.com>
2025-08-07 03:20:40 -0500

    feat(service): add Category service interface and implementation

---
+++ src/main/java/com/arka/inventory_service/service/ICategoryService.java
@@ -0,0 +1,14 @@
+package com.arka.inventory_service.service;
+
+import com.arka.inventory_service.dto.request.CategoryRequestDTO;
+import com.arka.inventory_service.dto.response.CategoryResponseDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ICategoryService {
+    CategoryResponseDTO createCategory(CategoryRequestDTO request);
+    CategoryResponseDTO getCategoryById(UUID id);
+    CategoryResponseDTO getCategoryByName(String name);
+    List<CategoryResponseDTO> getAllCategories();
+}
---
+++ src/main/java/com/arka/inventory_service/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,73 @@
+package com.arka.inventory_service.service.impl;
+
+import com.arka.inventory_service.dto.request.CategoryRequestDTO;
+import com.arka.inventory_service.dto.response.CategoryResponseDTO;
+import com.arka.inventory_service.exception.ResourceAlreadyExistsException;
+import com.arka.inventory_service.exception.ResourceNotFoundException;
+import com.arka.inventory_service.mapper.EntityToDTOMapper;
+import com.arka.inventory_service.model.Category;
+import com.arka.inventory_service.repository.CategoryRepository;
+import com.arka.inventory_service.service.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements ICategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final EntityToDTOMapper mapper;
+
+    @Override
+    public CategoryResponseDTO createCategory(CategoryRequestDTO request) {
+        return mapper.toDTO(createEntity(request));
+    }
+
+    @Override
+    public CategoryResponseDTO getCategoryById(UUID id) {
+        return mapper.toDTO(getCategoryByIdOrException(id));
+    }
+
+    @Override
+    public CategoryResponseDTO getCategoryByName(String name) {
+        Category category = categoryRepository.findByNameIgnoreCase(name)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found."));
+        return mapper.toDTO(category);
+    }
+
+    @Override
+    public List<CategoryResponseDTO> getAllCategories() {
+        return createResponseList(categoryRepository.findAll());
+    }
+
+    // --- Private utilitarian methods ---
+
+    private Category createEntity(CategoryRequestDTO request) {
+        validateUnique(request.getName());
+
+        Category category = new Category();
+        category.setName(request.getName());
+        category.setDescription(request.getDescription());
+
+        return categoryRepository.save(category);
+    }
+
+    private Category getCategoryByIdOrException(UUID id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category not found."));
+    }
+
+    private void validateUnique(String name) {
+        boolean exists = categoryRepository.existsByNameIgnoreCase(name);
+
+        if (exists) throw new ResourceAlreadyExistsException("Category name already exists.");
+    }
+
+    private List<CategoryResponseDTO> createResponseList(List<Category> categories) {
+        return categories.stream().map(mapper::toDTO).toList();
+    }
+}
