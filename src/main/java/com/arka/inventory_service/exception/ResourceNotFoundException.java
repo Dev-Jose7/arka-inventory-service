230564ffc00a108df8687ea794e1e35ee2459ec0
Jose Navarro <jfnr398@gmail.com>
2025-08-06 19:47:56 -0500

    feat(exception): add custom exception

---
+++ src/main/java/com/arka/inventory_service/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.arka.inventory_service.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(){
+        super("Resource not found.");
+    }
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
